97a0bde3fff3242a9bf14416d274e770ee6d07c0
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-02-20 10:31:55 -0700

    W5D1 Algo - Social Distancing

---
+++ Social_distancing.js
@@ -0,0 +1,34 @@
+const queue1 = [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1];
+const expected1 = false;
+
+const queue2 = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1];
+const expected2 = true;
+
+const queue3 = [1, 0, 0, 0, 0, 0, 0, 0, 1];
+const expected3 = true;
+
+const queue4 = [];
+const expected4 = true;
+
+/**
+ * Determines whether each occupied space in the line of people is separated by
+ * 6 empty spaces.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<0|1>} queue
+ * @returns {Boolean}
+ */
+function socialDistancingEnforcer(queue) {
+    let lastSeen = -1;
+    for (let i = 0; i < queue.length; i++) {
+        if (queue[i] === 1) {
+            if (lastSeen !== -1) {
+                if (i - lastSeen < 6) {
+                    return false;
+                }
+            }
+            lastSeen = i;
+        }
+    }
+    return true;
+}
